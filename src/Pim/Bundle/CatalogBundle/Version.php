ed8a73448028531f0d745f625e594c9511b3120a
Willy <willy.mesnage@akeneo.com>
2017-05-31 16:47:43 +0200

    Preparing version 1.7.110

--- src/Pim/Bundle/CatalogBundle/Version.php
+++ src/Pim/Bundle/CatalogBundle/Version.php
@@ -12,7 +12,7 @@ namespace Pim\Bundle\CatalogBundle;
 class Version
 {
     /** @staticvar string */
-    const VERSION = '1.7.106';
+    const VERSION = '1.7.110';
 
     /** @staticvar string */
     const VERSION_CODENAME = 'The Old Grey Hare';
